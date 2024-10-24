aca779b49f3f57d2686f2578ee8d40cb55684295
VicNsk <vicar@ngs.ru>
2024-10-24 10:57:24 +0700

    lesson3

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,11 +1,9 @@
 const bgElements = () => {
 	const bgElem = document.querySelectorAll('.set-bg')
-	for (let i = 0; i < bgElem.length; i++) {
-		const src = bgElem[i].dataset.setbg
-		// bgElem[i].style.backgroundImage = 'url(' + src + ')'
-		bgElem[i].style.backgroundImage = `url(${src})`
-		// console.log(src)
-	}
+
+	bgElem.forEach(elem => {
+		elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+	})
 }
 
 bgElements()
